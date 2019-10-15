49256bf3363062aba9d6c2e93db630b8b890371f
Jason Ish <ish@unx.ca>
2019-10-15 15:19:27 -0600

    entrypoing: don't export PS1
    I'm not sure why I was doing it this way, but it could
    prevent the shell from ever loading.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 # If the first command does not look like argument, assume its a
 # command the user wants to run. Normally I wouldn't do this.
 if [ "${1:0:1}" != "-" ]; then
-    PS1=${PS1} exec "$@"
+    exec "$@"
 fi
 
 for src in /etc/suricata.dist/*; do
