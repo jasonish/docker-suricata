5600bb64a252550e34631e461575e91cc686930d
Jason Ish <ish@unx.ca>
2020-09-12 11:39:19 -0600

    build.sh: rework to use plain docker
    Instead of buildx.

--- build.sh
+++ build.sh
@@ -1,11 +1,47 @@
 #! /bin/bash
 
 set -e
+set -x
 
 NAME="jasonish/suricata"
-TAG="master"
-DOCKER="docker"
-VERSION="${TAG}"
+VERSION="master"
+
+if [ "${HOST}" != "" ]; then
+    NAME="${HOST}/${NAME}"
+else
+    HOST="docker.io"
+fi
+
+builder="docker"
+build_command="build"
+
+for arg in $@; do
+    case "${arg}" in
+        push|--push)
+            push=yes
+            ;;
+        podman|--podman)
+            builder="podman"
+            ;;
+        --no-cache)
+            no_cache="--no-cache"
+            ;;
+        *)
+            echo "error: bad argument: ${arg}"
+            exit 1
+    esac
+done
+
+case "${builder}" in
+    podman)
+        if [ "${push}" = "yes" ]; then
+            echo "error: --push not supported with podman"
+            exit 1
+        fi
+    ;;
+    *)
+    ;;
+esac
 
 # Get the number of cores, defaulting to 2 if unable to get.
 cores=$(cat /proc/cpuinfo | grep ^processor | wc -l)
@@ -13,109 +49,37 @@ if [ "${cores}" = "0" ]; then
     cores=2
 fi
 
-# Flag to enable pushing on individual image build.
-do_push=no
-
-build() {
-    set -x
-    arch="$1"
-
-    case "${arch}" in
-        x86_64)
-            platform="linux/amd64"
-            ;;
-        armv6)
-            platform="linux/arm/v6"
-            ;;
-        *)
-            echo "error: bad platform: ${arch}"
-            exit 1
-            ;;
-    esac
-
-    args=()
-    if [ "${BUILDER_TAG}" ]; then
-        args+=(--cache-from=${BUILDER_TAG})
-        args+=(--tag=${BUILDER_TAG})
-    fi
-
-    docker buildx build --target builder ${args[@]} \
-           --load \
-           --platform ${platform} \
+${builder} ${build_command} \
+           ${build_opts} ${no_cache} \
+           --rm \
            --build-arg CORES="${cores}" \
-           -f Dockerfile.${arch} .
+           --tag ${NAME}:${VERSION}-amd64 \
+           -f Dockerfile.centos-amd64 \
+           .
 
-    args=()
-    if [ "${BUILDER_TAG}" ]; then
-        args+=(--cache-from=${BUILDER_TAG})
-    fi
-    if [ "${RUNNER_TAG}" ]; then
-        args+=(--cache-from=${RUNNER_TAG})
-    fi
-    docker buildx build ${args[@]} \
-           --load \
-           --target runner \
+${builder} ${build_command} \
+           ${build_opts} ${no_cache} \
+           --rm \
            --build-arg CORES="${cores}" \
-           --build-arg BUILD_DATE="$(date)" \
-           -t ${NAME}:${VERSION}-${arch} \
-           --platform ${platform} \
-           -f Dockerfile.${arch} .
+           --tag ${NAME}:${VERSION}-arm32v6 \
+           -f Dockerfile.alpine-arm32v6 \
+           .
 
-    if [ "${do_push}" = "yes" ]; then
-        push_image ${NAME}:${VERSION}-${arch}
+if [ "${push}" = "yes" ]; then
+    if [ "${builder}" = "docker" ]; then
+        docker push ${NAME}:${VERSION}-amd64
+        docker push ${NAME}:${VERSION}-arm32v6
+        
+        docker manifest create ${NAME}:${VERSION} \
+               -a ${NAME}:${VERSION}-amd64 \
+               -a ${NAME}:${VERSION}-arm32v6       
+        
+        docker manifest annotate --arch arm --variant v6 \
+               ${NAME}:${VERSION} ${NAME}:${VERSION}-arm32v6
+        
+        docker manifest push --purge ${NAME}:${VERSION}
+    else
+        echo "error: push no implemented for ${builder}"
+        exit 1
     fi
-}
-
-push_image() {
-    image="$1"
-    echo "Pushing ${image}"
-    docker push ${image}
-}
-
-push() {
-    # Push the individual images.
-    push_image "${NAME}:${VERSION}-x86_64"
-    push_image "${NAME}:${VERSION}-armv6"
-
-    manifests=(${NAME}:${VERSION})
-    if [[ "${TAG}" != "${VERSION}" ]]; then
-        manifests+=(${NAME}:${TAG})
-    fi
-    if test -e latest; then
-        manifests+=(${NAME}:latest)
-    fi
-    for manifest in ${manifests[@]}; do
-        docker manifest create ${manifest} \
-               -a ${NAME}:${VERSION}-x86_64 \
-               -a ${NAME}:${VERSION}-armv6
-    done
-    for manifest in ${manifests[@]}; do
-        docker manifest push -p ${manifest}
-    done
-}
-
-for arg in $@; do
-    case "${arg}" in
-        "--push")
-            do_push="yes"
-            shift
-            ;;
-    esac
-done
-
-case "$1" in
-    x86_64|amd64)
-        build x86_64
-        ;;
-    armv6)
-        build armv6
-        ;;
-    manifest|push)
-        push
-        ;;
-    all)
-        build x86_64
-        build armv6
-        push
-        ;;
-esac
+fi
